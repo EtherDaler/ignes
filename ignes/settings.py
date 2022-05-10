c709e222ad356b154b7acb68d8a032135bfb9db2
Daler Hojimatov <63519670+EtherDaler@users.noreply.github.com>
2022-05-10 23:14:59 +0300

    Update settings.py

--- ignes/settings.py
+++ ignes/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure--_ql4kt422y+3(_3^+)&nmmu%d8u4jp%qqpy3j!b$fub_3scy@'
-SECRET_KEY = os..environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
